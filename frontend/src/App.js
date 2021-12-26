263b00c1061d2c77680eb04382b2c4e7e4e9b332
WU,CHIEN YIN <wcy881218@gmail.com>
2021-12-26 16:04:30 +0800

    some change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useState} from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import 'antd/dist/antd.css';
 
 import LoginPage from './Containers/LoginPage';
@@ -11,26 +11,33 @@ import MainPage from './Containers/MainPage';
 
 
 function App() {
+
+
   
   const [me, setMe] = useState("")
   const [isLogin, setIsLogin] = useState(false);
+
+  function PrivateRoute({ children }) {
+    // const auth = useAuth();
+    return isLogin ? children : <Navigate to="/login" />;
+
+  }
+
+
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/' element={isLogin ? <MainPage /> : <LoginPage setIsLogin={setIsLogin} />}></Route>
-        <Route path='/login' element={<LoginPage setIsLogin={setIsLogin} />}></Route>
+        {/* public route */}
+        <Route path='/' element={isLogin ? <MainPage /> : <LoginPage setIsLogin={setIsLogin} />}></Route>
+        {/* private route */}
+        <Route path='/login' element={
+          <PrivateRoute>  
+            <LoginPage setIsLogin={setIsLogin} />
+          </PrivateRoute>}>
+        </Route>
         <Route path='/signUp' element={<SignUpPage />}></Route>
         <Route path='*' element={<PageNotFound />}></Route>
       </Routes>
-      {/* {
-        isLogin
-        ?
-        <h1>is Login</h1>
-        :
-        <>
-          <SignUpPage />
-        </>
-      } */}
       </BrowserRouter>
   );
 }
