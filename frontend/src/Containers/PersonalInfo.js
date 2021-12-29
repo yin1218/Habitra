4101edf6be07dc7ec532d964b440247ec5ecc90c
WU,CHIEN YIN <wcy881218@gmail.com>
2021-12-29 16:27:28 +0800

    init. some page

---
+++ frontend/src/Containers/PersonalInfo.js
@@ -0,0 +1,16 @@
+
+
+/* 
+1. 沒有編輯功能
+個人資料
+個人任務
+個人管理任務
+*/
+
+const PersonalInfo = () => {
+    return(
+        <h1>This is personal statistics page</h1>
+    )
+}
+
+export default PersonalInfo
--- frontend/src/Containers/PersonalStats.js
+++ frontend/src/Containers/PersonalStats.js
@@ -1,7 +1,7 @@
 const PersonalStats = () => {
     return(
-        <h1>This is personal statistics page</h1>
+        <h1>This is personal tasks statistics page</h1>
     )
 }
 
-export default PersonalStats
+export default PersonalStats;
